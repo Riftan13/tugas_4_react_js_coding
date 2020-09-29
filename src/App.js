8aaa8ce61278aa69975e079b4ead4316fad3702e
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-09-29 19:49:29 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuMakanan from './Page/MenuMakanan'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <MenuMakanan/>
+
+    </div>
+  );
+}
+
+export default App;
